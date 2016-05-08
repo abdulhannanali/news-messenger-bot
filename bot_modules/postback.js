d84ae5a382ca7c927286f82c553efbc605a807d3
Hannan Ali <abdulhannanali@outlook.com>
2016-05-08 14:31:29 +0500

    remove the /g flag from regexp in postback

--- bot_modules/postback.js
+++ bot_modules/postback.js
@@ -4,30 +4,35 @@ module.exports = function (bot) {
 
     bot.on("postback", function (data, reply) {
         var postback = data.postback.payload
-        console.log(postback)
         if (!(postback && typeof postback == "string" && postback.trim())) {
             return
         }  
         
         var splitParams = postback.split("_")
         var match;
-        if (postback.match(/GET_CATEGORIES/ig)) {
+        if (postback.match(/GET_CATEGORIES/i)) {
             newsBot.sendCategories(data, reply)
         }
-        else if (match = postback.match(/GET_CATEGORY_(.*)/ig)) {
+        else if (match = postback.match(/GET_CATEGORY_(.*)/i)) {
             newsBot.sendGenericNews(undefined, splitParams[2].toLowerCase(), data, reply)
         }
-        else if (postback.match(/GET_HELP/ig)) {
+        else if (postback.match(/GET_HELP/i)) {
             newsBot.sendHelp(data, reply)
         }
-        else if (postback.match(/GET_ABOUT/ig)) {
+        else if (postback.match(/GET_ABOUT/i)) {
             newsBot.sendAbout(data, reply)
         }
-        else if (match = postback.match(/GET_ARTICLE-RELATED_(.*)/)) {
+        else if (match = postback.match(/GET_ARTICLE-RELATED_(.*)/g)) {
             if (match && match[1] && match[1] != "undefined") {
                 newsBot.sendGenericNews(match[1], undefined, data, reply)    
             }
         }
-        // else if (match = postback.match(/ARTICLES_/ig)) {}
+        else if (match = postback.match(/ARTICLE_INFO_(.*)/i)) {
+            if (match[1]) {
+                var newsInfo = JSON.parse(match[1])
+                newsBot.sendNewsInfo(newsInfo, data, reply, console.log)
+            }
+        }
+        // else if (match = postback.match(/ARTICLES_/i)) {}
     })
 }
